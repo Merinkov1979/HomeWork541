869e60e41a48e62312d01595398951847c8ec6c3
Привет <mer79@internet.ru>
2022-10-30 22:26:21 +0300

    Initial commit

---
+++ src/main/java/com/example/homework541/HomeWork541Application.java
@@ -0,0 +1,13 @@
+package com.example.homework541;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork541Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork541Application.class, args);
+    }
+
+}
